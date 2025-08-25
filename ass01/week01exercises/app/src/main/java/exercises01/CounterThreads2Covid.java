9ad08c078af12051d04ff5ac4f4c8ea04dddfb6c
Marius Thomsen <mariu@itu.dk>
2025-08-25 12:30:47 +0200

    1.3 done

--- ass01/week01exercises/app/src/main/java/exercises01/CounterThreads2Covid.java
+++ ass01/week01exercises/app/src/main/java/exercises01/CounterThreads2Covid.java
@@ -2,23 +2,27 @@
 // raup@itu.dk * 2021-08-27
 package exercises01;
 
+import java.util.concurrent.locks.*;
+
 public class CounterThreads2Covid {
 
     long counter = 0;
-    final long PEOPLE  = 10_000;
+    final long PEOPLE = 10_000;
     final long MAX_PEOPLE_COVID = 15_000;
+    ReentrantLock lock = new ReentrantLock();
 
     public CounterThreads2Covid() {
         try {
             Turnstile turnstile1 = new Turnstile();
             Turnstile turnstile2 = new Turnstile();
 
-            turnstile1.start();turnstile2.start();
-            turnstile1.join();turnstile2.join();
+            turnstile1.start();
+            turnstile2.start();
+            turnstile1.join();
+            turnstile2.join();
 
-            System.out.println(counter+" people entered");
-        }
-        catch (InterruptedException e) {
+            System.out.println(counter + " people entered");
+        } catch (InterruptedException e) {
             System.out.println("Error " + e.getMessage());
             e.printStackTrace();
         }
@@ -28,11 +32,17 @@ public class CounterThreads2Covid {
         new CounterThreads2Covid();
     }
 
-
     public class Turnstile extends Thread {
         public void run() {
             for (int i = 0; i < PEOPLE; i++) {
-                counter++;
+                try {
+                    lock.lock();
+                    if (counter < MAX_PEOPLE_COVID)
+                        counter++;
+                } finally {
+                    lock.unlock();
+                }
+
             }
         }
     }
